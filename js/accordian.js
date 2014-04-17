da0b39275d40d727504c7d12e34e78160917a889
nlujan <naim.lujan@gmail.com>
2014-04-16 22:58:37 -0400

    modified width of accordian

--- js/accordian.js
+++ js/accordian.js
@@ -1,33 +1,40 @@
 $(function() {
 
-	// $("#accordian").zAccordion({
-	// 	timeout: 4000,
-	// 	slideWidth: 700,
-	// 	width: 1300,
-	// 	height: 700
-	// });
+	var openTabIndex;
+
 	$("#accordian").zAccordion({
 		startingSlide: 0,
 		auto: false,
-		// tabWidth: "5%",
 		slideWidth: "70%",
 		width: "100%",
 		height: 700,
 		trigger: "mousedown"
 	});
 
-	// $("#addButton").click(function(evt) {
-	// 	$("#accordian").append("<div><img src='images/slide3.gif' width='100%' height='100%' alt='' /></div>");
+	var tabIndex = function(){
+		var index = 0;
+		$("#accordian div").each(function(){
+			if ($(this).hasClass("frame-open")){
+				openTabIndex = index;
+			}
+			else{
+				index++;
+			}
+		});
+	};
+
+
 	var counter = 0
 	$("#addButton").click(function() {
 		var index = $("#accordian").children().length;
+		tabIndex();
 		$("#accordian").append("<div><img src='images/slide3.gif' width='100%' height='100%' alt='' /></div>");
 		$("#accordian").zAccordion("destroy", {
 			removeStyleAttr: true, /* This attribute will default to true and remove all inline styles. */
 			removeClasses: true, /* This attribute will default to false and remove any classes that have been set by zAccordion. */
 			destroyComplete: {
 				rebuild: {
-					startingSlide: index,
+					// startingSlide: openTabIndex,
 					auto: false,
 					slideWidth: "70%",
 					width: "100%",
@@ -36,8 +43,7 @@ $(function() {
 				}
 			}
 		});
-		//$("#addButton").zAccordion("trigger",index);
-		return false;
+		$("#accordian").zAccordion("trigger",index);
 	});
 
 
