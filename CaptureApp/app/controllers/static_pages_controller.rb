47596c2a590d18a5394cb92c1da235ef674b6ae4
Tochukwu Okoro <tcokoro@gmail.com>
2014-05-09 06:28:22 -0400

    home page

--- CaptureApp/app/controllers/static_pages_controller.rb
+++ CaptureApp/app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
+	before_action :logged_in
+
 	def home
+	end
+
+	def logged_in
+		if current_user == nil
+			redirect_to new_user_session_path
+		end
 	end
 end
